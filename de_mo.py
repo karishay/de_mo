fbcc3ba4148eba83a72785ab671337722c2c80b8
Shannon Burns <karishay@gmail.com>
2015-09-14 16:31:58 -0700

    base functionality works

--- de_mo.py
+++ de_mo.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 from flask import Flask, request, redirect, url_for
 from flask import render_template, flash, g
 from contextlib import closing
@@ -29,17 +30,36 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
+
 #==========# Routes #==========#
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/de_mo')
+@app.route('/', methods=['POST'])
 def de_motivate():
-    #query the database for a random insult
-    
-    #return template with name from form in index passed to insult from db
+    name = request.form['name']
+    print name
+    query_results = g.db.execute('SELECT words, too_mean from De_mos')
+    insults = [dict(words=row[0], too_mean=row[1]) for row in query_results.fetchall()]
+    random_insult= random.choice(insults)['words']
+    print "something"
+    return render_template('de_motivation.html', name=name, insult=random_insult)
+
+
+@app.route('/add_insults', methods=['GET'])
+def add_insults_view():
+    return render_template('add_insults.html')
+
+@app.route('/add_insults', methods=['POST'])
+def add_insults():
+    g.db.execute('insert into De_mos (words) values (?)',
+                [request.form['insult']])
+    g.db.commit()
+    flash("New insult added. Mwhahah!")
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
